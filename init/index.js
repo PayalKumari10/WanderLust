077c928a94b946cbfd91f06d05aa8d827944c4be
PayalKumari10 <kumaripayal7488@gmail.com>
2024-06-18 16:54:26 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const DB_URL = process.env.ATLASDB_URL;
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  //   await mongoose.connect(MONGO_URL);
+  await mongoose.connect(DB_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "65e412b492f2b1ff8fa49a0f",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+initDB();
